00654662d0f7eff5aea7451da6ffad25ee49b4ec
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-11 11:38:06 +0000

    book a space page added available from and to fields

--- app.rb
+++ app.rb
@@ -7,25 +7,24 @@ require './lib/space'
 # This is my new class
 class Makersbnb < Sinatra::Base
   enable :sessions
-  # get '/' do
-  #   erb :index
-  # end
 
   get '/add_a_listing' do
-    p "get add a listing"
     erb :'add_a_listing/index'
   end
 
   post '/add_a_listing' do
-    @space = Space.add(name: params[:name], description: params[:description], ppn: params[:ppn], start_date: params[:start_date], end_date: params[:end_date])
-    p "This is supposed to be a space#{@space}"
-    p "post add a listing"
+    @space = Space.add(
+      name: params[:name],
+      description: params[:description],
+      ppn: params[:ppn],
+      start_date: params[:start_date],
+      end_date: params[:end_date]
+    )
     redirect "/book_a_space"
   end
 
   get '/book_a_space' do
     @spaces = Space.all
-    p "get book a space"
     erb :'book_a_space/index'
   end
 
--- spec/features/book_a_space_page_spec.rb
+++ spec/features/book_a_space_page_spec.rb
@@ -1,4 +1,4 @@
-# feature 'book a space page' do
+feature 'book a space page' do
 #   scenario 'see all spaces' do
 #     visit '/add_a_listing'
 #     fill_in 'name', with: "Hairy Hotel"
@@ -11,4 +11,19 @@
 #     expect(page).to have_content "Hairy Hotel"
 #     expect(page).to have_content "Hairier Hotel"
 #   end
-# end
+  scenario 'button to go to list a space' do
+    visit '/book_a_space'
+    click_on('List a space')
+    expect(page).to have_current_path('/add_a_listing')
+  end
+  scenario 'list spaces within a range of dates' do
+    populate_test_table
+    visit '/book_a_space'
+    fill_in 'start_date', with: '2020-11-15'
+    fill_in 'end_date', with: '2020-11-18'
+    click_on('List Spaces')
+    expect(page).to have_content('Hairy Hotel')
+    expect(page).not_to have_content('Hairier Hotel')
+  end
+
+end
