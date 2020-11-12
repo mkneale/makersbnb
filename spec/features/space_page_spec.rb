4d0977ef92a0bb66bf7dc5e5b2a8d9569981bea8
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-12 09:29:43 +0000

    added buttons to space pages and added a css stylesheet to Public folder

--- spec/features/space_page_spec.rb
+++ spec/features/space_page_spec.rb
@@ -1,13 +1,36 @@
 feature 'space pages' do
-  scenario 'show space information' do
-    space = Space.add(name: 'Hairy Hotel',
+  before(:each) do
+    @space = Space.add(name: 'Hairy Hotel',
       description: 'Super hairy bro',
       ppn: 234,
       start_date: '2020-11-12',
-      end_date: '2020-11-28')
-    visit ("/book_a_space/#{space.id}")
-    expect(page).to have_content("#{space.name}")
-    expect(page).to have_content("#{space.description}")
+      end_date: '2020-11-28'
+    )
+  end
+  scenario 'show space information' do
+    visit ("/book_a_space/#{@space.id}")
+    expect(page).to have_content("#{@space.name}")
+    expect(page).to have_content("#{@space.description}")
     expect(page).to have_content('Pick a date:')
+  end
+  scenario 'spaces button should redirect to spaces page' do
+    visit "/book_a_space/#{@space.id}"
+    click_on 'Spaces'
+
+    expect(page).to have_current_path('/book_a_space')
+  end
+
+  scenario 'requests button should redirect to requests page' do
+    visit "/book_a_space/#{@space.id}"
+    click_on 'Requests'
+
+    expect(page).to have_current_path('/requests')
+  end
+
+  scenario 'sign out button should redirect to home page' do
+    visit "/book_a_space/#{@space.id}"
+    click_on 'Sign out'
+
+    expect(page).to have_current_path('/')
   end
 end
