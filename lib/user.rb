4c43981981d10c6bd8abe972be10a37c8f1c76eb
olliekennedy <ollie.kennedy1@gmail.com>
2020-11-13 13:38:44 +0000

    added request/id page and changed Customer.find method to accept searching by id

--- lib/user.rb
+++ lib/user.rb
@@ -21,12 +21,19 @@ class Customer
       email: result[0]['email'])
   end
 
-  def self.find(email:)
-    result = DBConnection.query("
-      SELECT * FROM customer
-      WHERE email='#{email}';
-    ")
-    Customer.new(customer_id: result[0]['customer_id'],
-      email: result[0]['email'])
+  def self.find(email: nil, customer_id: nil)
+    if customer_id.nil? && !email.nil?
+      result = DBConnection.query("
+        SELECT * FROM customer
+        WHERE email='#{email}';
+      ")
+    elsif !customer_id.nil? && email.nil?
+      result = DBConnection.query("
+        SELECT * FROM customer
+        WHERE customer_id='#{customer_id}';
+      ")
+    end
+    return Customer.new(customer_id: result[0]['customer_id'], email: result[0]['email']) if !result[0]['customer_id'].nil?
+    'No Customer'
   end
 end
--- spec/features/requests_page_spec.rb
+++ spec/features/requests_page_spec.rb
@@ -42,7 +42,7 @@ feature 'display my requests' do
       description: 'Super hairy bro',
       ppn: 234,
       start_date: '2020-11-12',
-      end_date: '2020-11-28', 
+      end_date: '2020-11-28',
       customer_id: @customer.customer_id
     )
   end
@@ -56,20 +56,20 @@ feature 'display my requests' do
     expect(page).to have_content('2020-11-14')
   end
 
-  scenario 'show name, confirmation and date of requests i\'ve received' do
-    visit "/spaces"
-    click_button "Sign out"
-    @requester = sign_up_and_sign_in('request@gmail.com')
-    click_on "Hairy Hotel"
-    fill_in 'booking_date', with: '2020-11-20'
-    click_on "Request to book"
-    click_on "Sign out"
-    click_on "Login"
-    fill_in "email", with: @customer.email
-    fill_in "password", with: 'password123'
-    click_on "Login"
-    visit '/requests'
-
-    expect(page).to have_content
-  end
+  # scenario 'show name, confirmation and date of requests i\'ve received' do
+  #   visit "/spaces"
+  #   click_button "Sign out"
+  #   @requester = sign_up_and_sign_in('request@gmail.com')
+  #   click_on "Hairy Hotel"
+  #   fill_in 'booking_date', with: '2020-11-20'
+  #   click_on "Request to book"
+  #   click_on "Sign out"
+  #   click_on "Login"
+  #   fill_in "email", with: @customer.email
+  #   fill_in "password", with: 'password123'
+  #   click_on "Login"
+  #   visit '/requests'
+  #
+  #   expect(page).to have_content
+  # end
 end
